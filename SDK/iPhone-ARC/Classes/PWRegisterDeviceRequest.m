df1f8bcd9649c41615b13e132963e7f135c65fd7
Max Konev <max@arello-mobile.com>
2012-11-30 15:19:15 +0700

    iOS: ARC version of the SDK

---
+++ SDK/iPhone-ARC/Classes/PWRegisterDeviceRequest.m
@@ -0,0 +1,29 @@
+//
+//  PWRegisterDeviceRequest
+//  Pushwoosh SDK
+//  (c) Pushwoosh 2012
+//
+
+#import "PWRegisterDeviceRequest.h"
+
+@implementation PWRegisterDeviceRequest
+@synthesize pushToken, language, timeZone;
+
+
+- (NSString *) methodName {
+	return @"registerDevice";
+}
+
+- (NSDictionary *) requestDictionary {
+	NSMutableDictionary *dict = [self baseDictionary];
+	
+	[dict setObject:[self encodeInt:1] forKey:@"device_type"];
+	[dict setObject:[self encodeString:pushToken] forKey:@"push_token"];
+	[dict setObject:[self encodeString:language] forKey:@"language"];
+	[dict setObject:[self encodeString:timeZone] forKey:@"timezone"];
+
+	return dict;
+}
+
+
+@end
